458a215d789afec31a2174d7e0daf0e8c3c51a3c
slu016 <dearsidi001@gmail.com>
2019-04-04 15:58:58 -0400

    init

---
+++ push.py
@@ -0,0 +1,24 @@
+import os
+
+git = "C:/Program Files/Git/git-cmd.exe"
+git_add = '"%s" add *'%git
+git_commit = '"%s" commit -m "test"'%git
+git_push = '"%s" push'%git
+# print(git_add)
+
+# print(os.getcwd())
+
+# 获取当前文件路径
+current_path = os.path.abspath(__file__)
+# 获取当前文件的父目录
+father_path = os.path.abspath(os.path.dirname(current_path) + os.path.sep + ".")
+
+os.chdir(father_path)
+print(os.getcwd())
+# os.system(git_add)
+# os.system(git_commit)
+# os.system(git_push)
+
+os.popen(git_add)
+os.popen(git_commit)
+os.popen(git_push)
